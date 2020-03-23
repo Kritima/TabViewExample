c68cf560dd025cdeaec527aba2ad69d0532ea1dd
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-03-23 18:19:29 -0400

    Image view added

--- TabViewExample/FirstViewController.swift
+++ TabViewExample/FirstViewController.swift
@@ -8,12 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tabBarItem.badgeValue = String(100)
     }
+    
+    
 
 
 }
